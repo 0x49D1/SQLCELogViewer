86607c0752577a75619f4ef66007d2327668553a
dpursanov <dpursanov@localhost>
2012-02-24 13:04:54 +0400

    Added export to CSV functionality

---
+++ Utilities/Export.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace SQLCELogViewer.Utilities
+{
+    public static class Export
+    {
+        public static void ExportToCSV(System.Windows.Controls.DataGrid dg)
+        {
+            if (dg.Items.Count == 0)
+                return;
+
+            dg.SelectAllCells();
+            dg.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+            ApplicationCommands.Copy.Execute(null, dg);
+            dg.UnselectAllCells();
+            String result = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "CSV (*.csv)|*.csv|All files (*.*)|*.*";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(ofd.FileName))
+                {
+                    file.WriteLine(result);
+                }
+            }
+        }
+    }
+}
--- Views/ShellView.cs
+++ Views/ShellView.cs
@@ -1,4 +1,6 @@
-﻿using MahApps.Metro.Controls;
+﻿using System.Windows.Input;
+using MahApps.Metro.Controls;
+using SQLCELogViewer.Utilities;
 
 namespace SQLCELogViewer
 {
@@ -7,6 +9,20 @@ namespace SQLCELogViewer
         public ShellView()
         {
             InitializeComponent();
+        }
+
+        private void MetroWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (((Keyboard.Modifiers & (ModifierKeys.Control | ModifierKeys.Shift)) != 0) && e.Key == Key.E)
+            {
+                Export.ExportToCSV(dgLog);
+            }
+        }
+
+        private void MetroWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
         }
     }
 }
