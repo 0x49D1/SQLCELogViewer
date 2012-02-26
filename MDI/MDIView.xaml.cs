fed1825fa2b60c21c9d90067a3e169afb2a39843
0x49D1 <0x49D1@localhost>
2012-02-26 12:47:49 +0400

    Restructuring. Added "MDI" TabControl

---
+++ MDI/MDIView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SQLCELogViewer.MDI
+{
+    /// <summary>
+    /// Interaction logic for MDIView.xaml
+    /// </summary>
+    public partial class MDIView : UserControl
+    {
+        public MDIView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MDI/MDIViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Caliburn.Micro;
+
+namespace SQLCELogViewer.MDI
+{
+    internal class MDIViewModel : Conductor<IScreen>.Collection.OneActive
+    {
+        public void Open(IScreen screen)
+        {
+            this.ActivateItem(screen);
+        }
+    }
+}
